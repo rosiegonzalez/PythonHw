2b5c62a2dc2d43e02c1b93393502bb0a733a0189
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-07-03 22:12:34 -0800

    homework

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
-//
+import React, { useState, useEffect} from 'react'
+ 
+function App() {
+
+    const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  //homework!!
+
+
+
+
+    return (
+      <div>
+
+        {(typeof data.members === 'undefined') ? (
+          <p>Loading...</p>
+        ): (
+          data.members.map((member, i) => (
+            <p key={i}>{member}</p>
+          ))
+        )}
+
+      </div>
+    )
+
+}
+
+export default App
